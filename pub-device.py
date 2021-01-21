d51179d06c3000f99164f4eec482e8824930fd45
MWarren1 <redemption.man@gmail.com>
2021-01-21 22:56:39 +0000

    Update and rename pub_device.py to pub-device.py

--- pub-device.py
+++ pub-device.py
@@ -1,9 +1,5 @@
 ### script that publishes devices stats to AWS
 ### Tested on Raspbian but should work on all linux based OS's
-### cmake, libssl-dev and AWS IoT SDK for Python v2 is needed install using the following command:
-### sudo apt-get install cmake
-### sudo apt-get install libssl-dev
-### pip install awsiotsdk
 
 ### Published message example ###
 # {
@@ -23,7 +19,23 @@ import json
 import os
 import awsconfig
 
-### Defining Fuctions
+# Define Topic to publish message to
+TOPIC = "test/device"
+# How often to publish  status in seconds
+SLEEPTIME = 10
+
+### Following defined by awsconfig.py file ###
+# AWS Endpoint
+ENDPOINT = awsconfig.ENDPOINT
+# Gets hostname
+CLIENT_ID = awsconfig.CLIENT_ID
+# Paths to Amazon CA, Private cert and key
+PATH_TO_CERT = awsconfig.PATH_TO_CERT
+PATH_TO_KEY = awsconfig.PATH_TO_KEY
+PATH_TO_ROOT = awsconfig.PATH_TO_ROOT
+### End of awsconfig.py variables ###
+
+### Defining Fuctions ###
 # Return CPU temperature as a character string, temp in 'C                                      
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
@@ -46,46 +58,38 @@ def getRAMinfo():
 def getCPUuse():
     return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\
 )))
-### End of Fuctions
 
-# Define Topic to publish message to
-TOPIC = "test/device"
-# How often to publish  status in seconds
-SLEEPTIME = 10
+def aws_publish_message(topic, message):
+    ### Start of creating MQTT connection
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+                endpoint=ENDPOINT,
+                cert_filepath=PATH_TO_CERT,
+                pri_key_filepath=PATH_TO_KEY,
+                client_bootstrap=client_bootstrap,
+                ca_filepath=PATH_TO_ROOT,
+                client_id=CLIENT_ID,
+                clean_session=False,
+                keep_alive_secs=6
+                )
+    print("Connecting to {} with client ID '{}'...".format(
+            ENDPOINT, CLIENT_ID))
+    # Make the connect() call
+    connect_future = mqtt_connection.connect()
+    # Future.result() waits until a result is available
+    connect_future.result()
+    #print("Connected!")
+    ### MQTT connection established
+    mqtt_connection.publish(topic=topic, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + topic)
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    return()                          
+### End of Fuctions ###
 
-# AWS Endpoint
-ENDPOINT = awsconfig.ENDPOINT
-# Paths to Amazon CA, Private cert and key
-PATH_TO_CERT = awsconfig.PATH_TO_CERT
-PATH_TO_KEY = awsconfig.PATH_TO_KEY
-PATH_TO_ROOT = awsconfig.PATH_TO_ROOT
-# Gets hostname
-CLIENT_ID = awsconfig.CLIENT_ID
 
-### Start of creating MQTT connection
-event_loop_group = io.EventLoopGroup(1)
-host_resolver = io.DefaultHostResolver(event_loop_group)
-client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
-mqtt_connection = mqtt_connection_builder.mtls_from_path(
-            endpoint=ENDPOINT,
-            cert_filepath=PATH_TO_CERT,
-            pri_key_filepath=PATH_TO_KEY,
-            client_bootstrap=client_bootstrap,
-            ca_filepath=PATH_TO_ROOT,
-            client_id=CLIENT_ID,
-            clean_session=False,
-            keep_alive_secs=6
-            )
-print("Connecting to {} with client ID '{}'...".format(
-        ENDPOINT, CLIENT_ID))
-# Make the connect() call
-connect_future = mqtt_connection.connect()
-# Future.result() waits until a result is available
-connect_future.result()
-print("Connected!")
-### MQTT connection established
-
-print('Begin Publish')
 forever = "true"
 while forever == "true":
     # Get current time/date
@@ -104,10 +108,8 @@ while forever == "true":
     # Get RAM Usage in %
     RAMstats = getRAMinfo()
     RAMusage = round((float(RAMstats[1])/float(RAMstats[0]))*100,1)
+    
+    # create message and publish
     message = {"key" : key, "date" : current_date, "time" : current_time, "hostname" : CLIENT_ID, "uptime" : uptime, "cputemp" : CPUtemp, "cpuusage" : CPUusge, "ramusage" : RAMusage }
-    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
-    print("Published: '" + json.dumps(message) + "' to the topic: " + TOPIC)
+    aws_publish_message(TOPIC, message)
     t.sleep(SLEEPTIME)
-print('Publish End')
-disconnect_future = mqtt_connection.disconnect()
-disconnect_future.result()
